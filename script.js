e6d2f532181dfc0c9843d07b449936f93149d95f
Andre Silva <37511945+mastersilvapt@users.noreply.github.com>
2022-01-18 14:16:36 +0000

    New Updates

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function average() {
         return;
     }
 
-    for (var i = 1; i < table.rows.length; i++) {
+    for (var i = 1; i < table.rows.length - 1; i++) {
         var cell = table.rows[i].cells[3];
         if (cell == null || cell.innerText == null || cell.innerText == "") continue;
         sum += parseInt(cell.innerText);
@@ -16,4 +16,18 @@ function average() {
     }
     var average = sum / div;
     document.getElementById("average").innerText = average.toString().substring(0, 4);
+}
+
+function toggle(button1, button2, elem) {
+    if (elem.style.display === "none") {
+        button1.style.display = "none"
+        button2.style.display = "block"
+        //button.innerText = "Hide";
+        elem.style.display = "block";
+    } else {
+        button1.style.display = "block"
+        button2.style.display = "none"
+        //button.innerText = "Show";
+        elem.style.display = "none";
+    }
 }
