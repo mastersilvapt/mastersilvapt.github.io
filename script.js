ddbd210d2eb0cedab9312f147862454d0c4a0514
André Silva <37511945+mastersilvapt@users.noreply.github.com>
2022-09-14 21:15:36 +0100

    Update To Master's Degree.

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
-function average() {
-    var table = document.getElementById('college_fcup_units');
-    var sumEtcs = 0;
-    var sumEtcsTimesGrade = 0;
+function average(inputId, outputId) {
+    const table = document.getElementById(inputId);
+    let sumEtc = 0;
+    let sumEtcTimesGrade = 0;
 
     if (table == null || table.rows == null) {
         console.log("table/rows null");
         return;
     }
 
-    for (var i = 1; i < table.rows.length - 1; i++) {
-        var etcs = table.rows[i].cells[3];
-        var cell = table.rows[i].cells[4];
-        if (cell == null || cell.innerText == null || cell.innerText == "") continue;
-        sumEtcsTimesGrade += parseInt(etcs.innerText) * parseInt(cell.innerText);
-        sumEtcs += parseInt(etcs.innerText);
+    for (let i = 1; i < table.rows.length - 1; i++) {
+        const etc = table.rows[i].cells[3];
+        const cell = table.rows[i].cells[4];
+        if (cell == null || cell.innerText == null || cell.innerText === "") continue;
+        sumEtcTimesGrade += parseInt(etc.innerText) * parseInt(cell.innerText);
+        sumEtc += parseInt(etc.innerText);
     }
-    var average = sumEtcsTimesGrade / sumEtcs;
-    document.getElementById("average").innerText = average.toString().substring(0, 5);
+    const average = sumEtcTimesGrade / sumEtc;
+    document.getElementById(outputId).innerText = isNaN(average) ? 0 : average.toString().substring(0, 5);
 }
 
-function etcs(){
-    var sumEtcs = 0;
-    var table = document.getElementById('college_fcup_units');
-    for (var i = 1; i < table.rows.length - 1; i++) {
-        var etcs = table.rows[i].cells[3];
-        if (etcs == null || etcs.innerText == null || etcs.innerText == "") continue;
-        sumEtcs += parseInt(etcs.innerText);
+function calcEtc(inputId, outputId){
+    let sumEtc = 0;
+    const table = document.getElementById(inputId);
+    for (let i = 1; i < table.rows.length - 1; i++) {
+        const etc = table.rows[i].cells[3];
+        if (etc == null || etc.innerText == null || etc.innerText === "") continue;
+        sumEtc += parseInt(etc.innerText);
     }
-    document.getElementById("etcs").innerText = sumEtcs.toString();
+    document.getElementById(outputId).innerText = sumEtc.toString();
 }
 
 function toggle(button1, button2, elem) {
-    if (elem.style.display === "none") {
-        button1.style.display = "none"
-        button2.style.display = "block"
+    const e = document.getElementById(elem);
+    const bt1 = document.getElementById(button1);
+    const bt2 = document.getElementById(button2);
+    if (e.style.display === "none") {
+        bt1.style.display = "none"
+        bt2.style.display = "block"
         //button.innerText = "Hide";
-        elem.style.display = "block";
+        e.style.display = "block";
     } else {
-        button1.style.display = "block"
-        button2.style.display = "none"
+        bt1.style.display = "block"
+        bt2.style.display = "none"
         //button.innerText = "Show";
-        elem.style.display = "none";
+        e.style.display = "none";
     }
 }
